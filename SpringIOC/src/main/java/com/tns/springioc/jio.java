4d144fc01a46f77801453db72261432919f181e1
hp <hp@LAPTOP-44LCNLGG>
2022-06-09 23:32:29 +0530

    first commit

---
+++ SpringIOC/src/main/java/com/tns/springioc/jio.java
@@ -0,0 +1,15 @@
+package com.tns.springioc;
+
+public class jio implements Sim{
+
+	public void calling() {
+		System.out.println("Calling using jio sim");
+		
+	}
+
+	public void data() {
+		System.out.println("data using jio sim");
+		
+	}
+
+}
