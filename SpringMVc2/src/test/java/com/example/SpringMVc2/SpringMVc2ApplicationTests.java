2081ed8b63e357d69add83cf53462dfe2ed49609
hp <hp@LAPTOP-44LCNLGG>
2022-06-29 12:19:57 +0530

    first commit

---
+++ SpringMVc2/src/test/java/com/example/SpringMVc2/SpringMVc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMVc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
